a2e886698d48779f192b45065549e2817b0a435d
hdongqt <hdongqt@gmail.com>
2022-10-11 16:29:19 +0700

    fix(Module 5): "change structure code"

--- kanban-board/src/routes/MainRoutes.jsx
+++ kanban-board/src/routes/MainRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { BoardActive, BackLog } from "../features/Board";
+import { BoardActive, BackLog } from "../containers/Board/index";
 
 const MainRoutes = () => {
   return (
