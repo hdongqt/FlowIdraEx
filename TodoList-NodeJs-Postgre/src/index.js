6cfc92c10fcbff0aad740524222363ecbdce10cd
hdongqt <hdongqt@gmail.com>
2022-10-13 15:18:58 +0700

    fix(Module3): "add middleware CROS"

--- TodoList-NodeJs-Postgre/src/index.js
+++ TodoList-NodeJs-Postgre/src/index.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 //routes
 app.use(require('./routes'));
 
