19b1a5a8f772e8c7612cbeb4d6d09f9150d2f24e
hdongqt <hdongqt@gmail.com>
2022-10-17 17:05:19 +0700

    feat(Module3): "add route backend kanbanboard"

--- KanbanBoard-NodeJs-Postgre/src/controllers/index.controller.js
+++ KanbanBoard-NodeJs-Postgre/src/controllers/index.controller.js
@@ -1,4 +1,4 @@
-const {Pool} = require('pg');
+const {Pool} = require("pg");
 
 const pool = new Pool({
     user: "postgres",
@@ -6,20 +6,120 @@ const pool = new Pool({
     database: "kanbanboard",
     password: "123456",
     port: 5432,
-})
+});
 
 const getTasks = async (req, res) => {
     try {
-        const response = await pool.query(`SELECT id, title, description, status, issue_type, priority_type, reporter_id,
-        assignee_id FROM tasks where is_delete = false ORDER BY updated_At ASC`);
+        const {search, status} = req.query;
+        let response;
+        if (status === "backlog") {
+            const query = `SELECT id, title, description, status, issue_type, priority_type, reporter_id, assignee_id 
+        FROM tasks WHERE is_delete = false AND title iLIKE '%'||$1||'%' AND status = 'BACKLOG'
+        ORDER BY updated_At ASC`;
+            response = await pool.query(query, [search]);
+        } else {
+            const query = `SELECT id, title, description, status, issue_type, priority_type, reporter_id, assignee_id 
+        FROM tasks WHERE is_delete = false AND title iLIKE '%'||$1||'%' ORDER BY updated_At ASC`;
+            response = await pool.query(query, [search]);
+        }
         res.status(200).json(response.rows);
     } catch (error) {
         res.json({
-            message: "Error: " + error
+            message: "Error: " + error,
         });
     }
 };
 
+const getTaskById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const response = await pool.query(
+            `SELECT id, title, description, status, issue_type, priority_type, reporter_id,
+        assignee_id FROM tasks where is_delete = false and id = $1`,
+            [id]
+        );
+        if (response.rows.length > 0) {
+            res.status(200).json(response.rows);
+        } else {
+            res.status(404).json({message: "Item requested was not found"});
+        }
+    } catch (error) {
+        res.json({
+            message: "Error: " + error,
+        });
+    }
+};
+
+const createTask = async (req, res) => {
+    try {
+        const {title, description, status, issue_type, priority_type, reporter_id, assignee_id} = req.body;
+        const query = `INSERT INTO tasks(title, description, status, issue_type, priority_type, reporter_id,
+        assignee_id) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
+        const response = await pool.query(query, [
+            title,
+            description,
+            status,
+            issue_type,
+            priority_type,
+            reporter_id,
+            assignee_id,
+        ]);
+        res.status(201).json({
+            message: "Task Add Successfully",
+        });
+    } catch (error) {
+        res.json({
+            message: "Error: " + error,
+        });
+    }
+};
+
+const deleteTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const selectTask = await pool.query('SELECT * FROM tasks WHERE id = $1 and "is_delete" = false', [id]);
+        if (selectTask.rows.length > 0) {
+            const response = await pool.query('UPDATE tasks SET "is_delete" = true  WHERE id= $1', [id]);
+            res.status(200).json({
+                message: `Task deleted successfully`,
+            });
+        } else {
+            res.status(404).json({message: "Task requested was not found"});
+        }
+    } catch (error) {
+        res.json({
+            message: "Error: " + error,
+        });
+    }
+};
+
+const updateTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const {title, description, status, issue_type, priority_type, reporter_id, assignee_id} = req.body;
+        const selectTask = await pool.query('SELECT * FROM tasks WHERE id = $1 and "is_delete" = false', [id]);
+        if (selectTask.rows.length > 0) {
+            const response = await pool.query(
+                `UPDATE tasks SET title=$1, description=$2, status=$3, issue_type=$4, priority_type=$5,
+        reporter_id=$6, assignee_id=$7 WHERE id = $8`,
+                [title, description, status, issue_type, priority_type, reporter_id, assignee_id, id]
+            );
+            res.status(200).json({
+                message: "Todo updated successfully",
+            });
+        } else {
+            res.status(404).json({message: "Item requested was not found"});
+        }
+    } catch (error) {
+        res.json({
+            message: "Error: " + error,
+        });
+    }
+};
 module.exports = {
     getTasks,
-}
+    getTaskById,
+    createTask,
+    deleteTask,
+    updateTask,
+};
--- KanbanBoard-NodeJs-Postgre/src/routes/index.js
+++ KanbanBoard-NodeJs-Postgre/src/routes/index.js
@@ -1,9 +1,12 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const {getTasks} = require('../controllers/index.controller');
+const { getTasks, getTaskById, createTask, deleteTask, updateTask } = require("../controllers/index.controller");
 
-router.get('/api/kanbanboard', getTasks);
-
+router.get("/api/kanbanboard", getTasks);
+router.get("/api/kanbanboard/:id", getTaskById);
+router.post("/api/kanbanboard", createTask);
+router.delete("/api/kanbanboard/:id", deleteTask);
+router.put("/api/kanbanboard/:id", updateTask);
 
 module.exports = router;
