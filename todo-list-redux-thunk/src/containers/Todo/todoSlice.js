e86d22cff3f08d70117d199ec0ba38824c0941be
hdongqt <hdongqt@gmail.com>
2022-10-17 08:56:54 +0700

    feat(Module3): "fix enter add task not show alert"

--- todo-list-redux-thunk/src/containers/Todo/todoSlice.js
+++ todo-list-redux-thunk/src/containers/Todo/todoSlice.js
@@ -57,65 +57,78 @@ const todoSlice = createSlice({
 });
 
 // create thunk action
-export const getListTodo = createAsyncThunk("todos/getListTodo", async () => {
-    const response = await fetchTodoAPI();
-    const data = await response.json();
-    return data;
-});
-
-export const addNewTodoFilter = createAsyncThunk("todos/addNewTodoFilter", async (todo, thunkAPI) => {
-    try {
-        if (todo.title.includes("từ bậy")) todo.title = "Công việc vui vẻ";
-        const response = await createTodoAPI(todo);
-        const data = await response.json();
-        if (response.status !== 201) {
-            message.error(data.message);
-            return thunkAPI.rejectWithValue(data);
+export const getListTodo = createAsyncThunk("todos/getListTodo",
+    async (thunkAPI, ...params) => {
+        try {
+            const response = await fetchTodoAPI();
+            const data = await response.json();
+            if (response.status !== 200) {
+                message.error(data.message);
+                return thunkAPI.rejectWithValue(data);
+            }
+            return data;
+        } catch (e) {
+            message.error(e.message);
+            return thunkAPI.rejectWithValue(e);
         }
-        thunkAPI.dispatch(getListTodo());
-        message.success(data.message);
-        return data;
-    } catch (error) {
-        message.error(error.message);
-        return thunkAPI.rejectWithValue(error.message);
-    }
-});
+    });
 
-export const editTodoFilter = createAsyncThunk("todos/editTodoFilter", async (todo, thunkAPI) => {
-    try {
-        if (todo.title.includes("từ bậy")) todo.title = "Công việc vui vẻ";
-        const response = await updateTodoAPI(todo);
-        const data = await response.json();
-        thunkAPI.dispatch(getListTodo());
-        if (response.status !== 200) {
-            message.error(data.message);
-            return thunkAPI.rejectWithValue(data);
+export const addNewTodoFilter = createAsyncThunk("todos/addNewTodoFilter",
+    async (todo, thunkAPI) => {
+        try {
+            if (todo.title.includes("từ bậy")) todo.title = "Công việc vui vẻ";
+            const response = await createTodoAPI(todo);
+            const data = await response.json();
+            if (response.status !== 201) {
+                message.error(data.message);
+                return thunkAPI.rejectWithValue(data);
+            }
+            thunkAPI.dispatch(getListTodo());
+            message.success(data.message);
+            return data;
+        } catch (error) {
+            message.error(error.message);
+            return thunkAPI.rejectWithValue(error.message);
         }
-        thunkAPI.dispatch(getListTodo());
-        message.success(data.message);
-        return data;
-    } catch (error) {
-        message.error(error);
-        return thunkAPI.rejectWithValue(error.message);
-    }
-});
+    });
 
-export const deleteTodo = createAsyncThunk("todos/deleteTodo", async (id, thunkAPI) => {
-    try {
-        const response = await deleteTodoAPI(id);
-        const data = await response.json();
-        if (response.status !== 200) {
-            message.error(data.message);
-            return thunkAPI.rejectWithValue(data);
+export const editTodoFilter = createAsyncThunk("todos/editTodoFilter",
+    async (todo, thunkAPI) => {
+        try {
+            if (todo.title.includes("từ bậy")) todo.title = "Công việc vui vẻ";
+            const response = await updateTodoAPI(todo);
+            const data = await response.json();
+            thunkAPI.dispatch(getListTodo());
+            if (response.status !== 200) {
+                message.error(data.message);
+                return thunkAPI.rejectWithValue(data);
+            }
+            thunkAPI.dispatch(getListTodo());
+            message.success(data.message);
+            return data;
+        } catch (error) {
+            message.error(error);
+            return thunkAPI.rejectWithValue(error.message);
         }
-        thunkAPI.dispatch(getListTodo());
-        message.success(data.message);
-        return data;
-    } catch (error) {
-        message.error(error.message);
-        return thunkAPI.rejectWithValue(error.message);
-    }
-});
+    });
+
+export const deleteTodo = createAsyncThunk("todos/deleteTodo",
+    async (id, thunkAPI) => {
+        try {
+            const response = await deleteTodoAPI(id);
+            const data = await response.json();
+            if (response.status !== 200) {
+                message.error(data.message);
+                return thunkAPI.rejectWithValue(data);
+            }
+            thunkAPI.dispatch(getListTodo());
+            message.success(data.message);
+            return data;
+        } catch (error) {
+            message.error(error.message);
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    });
 
 // export reducer and actions
 const {reducer, actions} = todoSlice;
