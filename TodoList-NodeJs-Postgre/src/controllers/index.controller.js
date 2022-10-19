335e06a92ec90fd4f043edae22d771d84324ebe4
hdongqt <hdongqt@gmail.com>
2022-10-19 08:45:10 +0700

    fix(Module3): "add middleware handle error backend todolist"

--- TodoList-NodeJs-Postgre/src/controllers/index.controller.js
+++ TodoList-NodeJs-Postgre/src/controllers/index.controller.js
@@ -1,116 +1,112 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "todolist",
-    password: "123456",
-    port: 5432,
-})
+  user: "postgres",
+  host: "localhost",
+  database: "todolist",
+  password: "123456",
+  port: 5432,
+});
 
-const getTodos = async (req, res) => {
-    try {
-        const response = await pool.query(`SELECT id,title,"isDone" FROM todos where "isDelete" = false ORDER BY "updatedAt" ASC`);
-        res.status(200).json(response.rows);
-    } catch (error) {
-        res.json({
-            message: "Error: " + error
-        });
-    }
+const getTodos = async (req, res, next) => {
+  try {
+    const response = await pool.query(
+      `SELECT id,title,"isDone" FROM todos where "isDelete" = false ORDER BY "updatedAt" ASC`
+    );
+    res.status(200).json(response.rows);
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-const getTodoById = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const response = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
-        if (response.rows.length > 0) {
-            res.status(200).json(response.rows);
-        } else {
-            res.status(404).json({message: 'Item requested was not found'});
-        }
-    } catch (error) {
-        res.json({
-            message: "Error: " + error
-        });
+const getTodoById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
+    if (response.rows.length > 0) {
+      res.status(200).json(response.rows);
+    } else {
+      next(responseError("Item requested was not found", 404));
     }
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-const createTodo = async (req, res) => {
-    try {
-        const {title, isDone} = req.body;
-        let errorMessage = ""
-        if (title.toLowerCase().includes('fuck')) {
-            errorMessage += "Title is not allow "
-        }
-        if (title.length > 25) {
-            errorMessage += "Title no more than 25 characters; "
-        }
-        if (errorMessage.length > 0) {
-            res.status(400).json({message: errorMessage})
-        } else {
-            const response = await pool.query('INSERT INTO todos(title, "isDone") VALUES($1, $2)', [title, isDone]);
-            res.status(201).json({
-                message: 'Todo Add Successfully',
-                data: {
-                    todo: {title, isDone}
-                }
-            });
-        }
-    } catch (error) {
-        res.json({
-            message: "Error: " + error
-        });
+const createTodo = async (req, res, next) => {
+  try {
+    const { title, isDone } = req.body;
+    let errorMessage = "";
+    if (title.toLowerCase().includes("fuck")) {
+      errorMessage += "Title is not allow ";
     }
+    if (title.length > 25) {
+      errorMessage += "Title no more than 25 characters; ";
+    }
+    if (errorMessage.length > 0) {
+      next(responseError(errorMessage, 400));
+    } else {
+      const response = await pool.query('INSERT INTO todos(title, "isDone") VALUES($1, $2)', [title, isDone]);
+      res.status(201).json({
+        message: "Todo Add Successfully",
+        data: {
+          todo: { title, isDone },
+        },
+      });
+    }
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-const deleteTodo = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const selectTodo = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
-        if (selectTodo.rows.length > 0) {
-            const response = await pool.query('UPDATE todos SET "isDelete" = true  WHERE id= $1', [id]);
-            res.status(200).json({
-                message: `Todo deleted successfully`
-            });
-        } else {
-            res.status(404).json({message: 'Item requested was not found'});
-        }
-
-    } catch (error) {
-        res.json({
-            message: "Error: " + error
-        });
+const deleteTodo = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const selectTodo = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
+    if (selectTodo.rows.length > 0) {
+      const response = await pool.query('UPDATE todos SET "isDelete" = true  WHERE id= $1', [id]);
+      res.status(200).json({
+        message: `Todo deleted successfully`,
+      });
+    } else {
+      next(responseError("Item requested was not found", 404));
     }
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-const updateTodo = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const {title, isDone} = req.body;
-        if (title.includes('fuck')) {
-            res.status(400).json({message: 'Title is not allow'})
-        }
-        const selectTodo = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
-        if (selectTodo.rows.length > 0) {
-            const response = await pool.query('UPDATE todos SET title = $1, "isDone"=$2 WHERE id = $3', [title, isDone, id]);
-            res.status(200).json({
-                message: 'Todo updated successfully'
-            });
-        } else {
-            res.status(404).json({message: 'Item requested was not found'});
-        }
-
-    } catch (error) {
-        res.json({
-            message: "Error: " + error
-        });
+const updateTodo = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { title, isDone } = req.body;
+    if (title.includes("fuck")) {
+      next(responseError("Title is not allow", 400));
     }
-}
+    const selectTodo = await pool.query('SELECT * FROM todos WHERE id = $1 and "isDelete" = false', [id]);
+    if (selectTodo.rows.length > 0) {
+      const response = await pool.query('UPDATE todos SET title = $1, "isDone"=$2 WHERE id = $3', [title, isDone, id]);
+      res.status(200).json({
+        message: "Todo updated successfully",
+      });
+    } else {
+      next(responseError("Item requested was not found", 404));
+    }
+  } catch (error) {
+    next(new Error(error));
+  }
+};
+
+const responseError = (message, code = 500) => {
+  const error = new Error(message);
+  error.status = code;
+  return error;
+};
 
 module.exports = {
-    getTodos,
-    getTodoById,
-    createTodo,
-    updateTodo,
-    deleteTodo
-}
+  getTodos,
+  getTodoById,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
--- TodoList-NodeJs-Postgre/src/index.js
+++ TodoList-NodeJs-Postgre/src/index.js
@@ -1,20 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", " GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", " GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 //routes
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-app.listen(3300, () => {
-    console.log('listening on port 3300');
+//middlewares handle error response
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.json({
+    error: {
+      status: err.status,
+      message: err.message,
+    },
+  });
 });
 
+app.listen(3300, () => {
+  console.log("listening on port 3300");
+});
